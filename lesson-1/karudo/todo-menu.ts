43d2d24f38452fe108d086be20b7179863dc0444
Deonis Peretyagin <karudo@gmail.com>
2016-09-24 22:58:36 +0700

    karudo homework 1

---
+++ lesson-1/karudo/todo-menu.ts
@@ -0,0 +1,72 @@
+type menuElement = {
+    title: string,
+    items?: menuElement[]
+};
+
+const menu = [
+    {
+        title: 'Животные',
+        items: [
+            {
+                title: 'Млекопитающие',
+                items: [
+                    {title: 'Коровы'},
+                    {title: 'Ослы'},
+                    {title: 'Собаки'},
+                    {title: 'Тигры'}
+                ]
+            },
+            {
+                title: 'Другие',
+                items: [
+                    {title: 'Змеи'},
+                    {title: 'Птицы'},
+                    {title: 'Ящерицы'},
+                ],
+            },
+        ]
+    },
+    {
+        title: 'Рыбы', items: [
+        {
+            title: 'Аквариумные',
+            items: [
+                {title: 'Гуппи'},
+                {title: 'Скалярии'}
+            ]
+        },
+        {
+            title: 'Форель',
+            items: [
+                {title: 'Морская форель'}
+            ]
+        },
+    ]
+    }
+];
+
+
+function generateMenu(list: menuElement[]): string {
+    let str: string = `<ul>`;
+    for (let a of list) {
+        str += `<li><a class="title">${a.title}</a>`;
+        if (a.items) {
+            str += generateMenu(a.items);
+        }
+        str += `</li>`
+    }
+    str += `</ul>`;
+    return str;
+}
+
+let navMenuList = document.querySelector(".menu") as HTMLElement;
+navMenuList.innerHTML = generateMenu(menu);
+navMenuList.onclick = (ev: MouseEvent) => {
+    let el = ev.target as HTMLElement;
+    let classList = el.classList;
+    if (!classList.contains('title')) {
+        return;
+    }
+    let parentLi = el.parentNode as HTMLLIElement;
+    parentLi.classList.toggle("menu-open")
+};
