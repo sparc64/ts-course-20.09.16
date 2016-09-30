97a6342d6103a137d31a7da8e379dfcc5929e8e8
Nepipenkoigor <igornepipenko@gmail.com>
2016-09-30 16:08:40 +0300

    add lesson 3

---
+++ lesson-3/flickr/scripts/fetch.ts
@@ -0,0 +1,9 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/GlobalFetch/fetch
+// https://github.com/iliakan/ts-course/blob/master/ts-lesson-3/demo-flikr-app/scripts/fetch.ts
+/**
+ *  uri: 'https://api.flickr.com/services/rest/?',
+ * queryMethod: 'flickr.photos.search',
+ * apiKey: '7fbc4d0fd04492d32fa9a2f718c6293e'
+ */
+
+
